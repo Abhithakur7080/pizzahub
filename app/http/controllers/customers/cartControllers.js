ba216451d07b86b80a11beccbc56908511b4e823
Abhijeet Kumar <abhijeetthakur7080@gmail.com>
2024-06-06 19:11:08 +0530

    cart controllers some changes

--- app/http/controllers/customers/cartControllers.js
+++ app/http/controllers/customers/cartControllers.js
@@ -1,39 +1,33 @@
 const cartControllers = () => {
-    const index = (req, res) => {
-        res.render("customers/cart");
-    };
+  return {
+    index(req, res) {
+      res.render("customers/cart");
+    },
+    update(req, res) {
+      if (!req.session.cart) {
+        req.session.cart = {
+          items: {},
+          totalQty: 0,
+          totalPrices: 0,
+        };
+      }
+      const cart = req.session.cart;
 
-    const update = (req, res) => {
-        if (!req.session.cart) {
-            req.session.cart = {
-                items: {},
-                totalQty: 0,
-                totalPrices: 0
-            };
-        }
-        const cart = req.session.cart;
-
-        if (!cart.items[req.body._id]) {
-            cart.items[req.body._id] = {
-                item: req.body,
-                qty: 1
-            };
-            cart.totalQty += 1;
-            cart.totalPrices += req.body.price;
-        } else {
-            cart.items[req.body._id].qty += 1;
-            cart.totalQty += 1;
-            cart.totalPrices += req.body.price;
-        }
-
-        return res.json({ totalQty: req.session.cart.totalQty });
-    };
-
-    return {
-        index,
-        update
-    };
+      if (!cart.items[req.body._id]) {
+        cart.items[req.body._id] = {
+          item: req.body,
+          qty: 1,
+        };
+        cart.totalQty += 1;
+        cart.totalPrices += req.body.price;
+      } else {
+        cart.items[req.body._id].qty += 1;
+        cart.totalQty += 1;
+        cart.totalPrices += req.body.price;
+      }
+      return res.json({ totalQty: req.session.cart.totalQty });
+    },
+  };
 };
-
 
 export default cartControllers;
